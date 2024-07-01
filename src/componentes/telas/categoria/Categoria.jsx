ae90cc911625cabdcd970626c2c87dc25dadead4
Jorge Luis Boeira Bavaresco <jorge@jorgedebian.ifsul>
2024-07-01 11:21:39 -0300

    validação e componente carregando

--- src/componentes/telas/categoria/Categoria.jsx
+++ src/componentes/telas/categoria/Categoria.jsx
@@ -1,30 +1,33 @@
 import { useState, useEffect } from "react";
 import CategoriaContext from "./CategoriaContext";
-import { getCategoriasAPI, getCategoriaPorCodigoAPI, 
-deleteCategoriaAPI, cadastraCategoriaAPI } from "../../../servicos/CategoriaServico";
+import {
+    getCategoriasAPI, getCategoriaPorCodigoAPI,
+    deleteCategoriaAPI, cadastraCategoriaAPI
+} from "../../../servicos/CategoriaServico";
 import Tabela from "./Tabela";
 import Form from "./Form";
+import Carregando from "../../comuns/Carregando";
 
-function Categoria(){
+function Categoria() {
 
-    const [alerta, setAlerta] = useState({ status : "", message : ""});
+    const [alerta, setAlerta] = useState({ status: "", message: "" });
     const [listaObjetos, setListaObjetos] = useState([]);
     const [editar, setEditar] = useState(false);
-    const [objeto, setObjeto] = useState({ codigo : "", nome : ""});
+    const [objeto, setObjeto] = useState({ codigo: "", nome: "" });
 
     const novoObjeto = () => {
         setEditar(false);
-        setAlerta({status : "", message : ""});
+        setAlerta({ status: "", message: "" });
         setObjeto({
-            codigo : 0, 
-            nome : ""
+            codigo: 0,
+            nome: ""
         });
     }
 
     const editarObjeto = async codigo => {
         setObjeto(await getCategoriaPorCodigoAPI(codigo));
         setEditar(true);
-        setAlerta({status : "", message :""});
+        setAlerta({ status: "", message: "" });
     }
 
     const acaoCadastrar = async e => {
@@ -32,9 +35,9 @@ function Categoria(){
         const metodo = editar ? "PUT" : "POST";
         try {
             let retornoAPI = await cadastraCategoriaAPI(objeto, metodo);
-            setAlerta({status : retornoAPI.status, message : retornoAPI.message});
+            setAlerta({ status: retornoAPI.status, message: retornoAPI.message });
             setObjeto(retornoAPI.objeto);
-            if (!editar){
+            if (!editar) {
                 setEditar(true);
             }
         } catch (err) {
@@ -46,32 +49,38 @@ function Categoria(){
     const handleChange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        setObjeto({...objeto, [name] : value});
+        setObjeto({ ...objeto, [name]: value });
     }
 
+    const [carregando, setCarregando] = useState(false);
+
     const recuperaCategorias = async () => {
+        setCarregando(true);
         setListaObjetos(await getCategoriasAPI());
+        setCarregando(false);
     }
 
     const remover = async codigo => {
-        if (window.confirm('Deseja remover este objeto?')){
+        if (window.confirm('Deseja remover este objeto?')) {
             let retornoAPI = await deleteCategoriaAPI(codigo);
-            setAlerta({ status : retornoAPI.status, message : retornoAPI.message});
+            setAlerta({ status: retornoAPI.status, message: retornoAPI.message });
             recuperaCategorias();
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         recuperaCategorias();
-    },[]);
+    }, []);
 
     return (
         <CategoriaContext.Provider value={{
             alerta, listaObjetos, remover,
             objeto, acaoCadastrar, handleChange, novoObjeto, editarObjeto
         }}>
-            <Tabela/>
-            <Form/>
+            <Carregando carregando={carregando}>
+                <Tabela />
+            </Carregando>
+            <Form />
         </CategoriaContext.Provider>
     )
 
--- src/componentes/telas/categoria/Form.jsx
+++ src/componentes/telas/categoria/Form.jsx
@@ -6,6 +6,25 @@ function Form() {
 
     const { objeto, handleChange, acaoCadastrar, alerta } = useContext(CategoriaContext);
 
+    (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })()
+
     return (
         <div className="modal fade" id="modalEdicao" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog">
@@ -14,28 +33,36 @@ function Form() {
                         <h1 className="modal-title fs-5" id="exampleModalLabel">Categoria</h1>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <form id="formulario" onSubmit={acaoCadastrar}>
+                    <form id="formulario" onSubmit={acaoCadastrar}
+                        className="needs-validation" noValidate>
                         <div className="modal-body">
+                            <Alerta alerta={alerta} />
                             <div className="mb-3">
-                                <label htmlFor="txtCodigo" 
-                                className="form-label">Código</label>
-                                <input type="number" class="form-control" 
-                                id="txtCodigo"
-                                readOnly
-                                name="codigo"
-                                value={objeto.codigo}
-                                onChange={handleChange} />
+                                <label htmlFor="txtCodigo"
+                                    className="form-label">Código</label>
+                                <input type="number" class="form-control"
+                                    id="txtCodigo"
+                                    readOnly
+                                    name="codigo"
+                                    value={objeto.codigo}
+                                    onChange={handleChange} />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="txtNome" 
-                                className="form-label">Nome</label>
-                                <input type="text" class="form-control" 
-                                id="txtNome" placeholder="Informe o nome"
-                                required
-                                name="nome"
-                                value={objeto.nome}
-                                onChange={handleChange} />
-                            </div>                            
+                                <label htmlFor="txtNome"
+                                    className="form-label">Nome</label>
+                                <input type="text" class="form-control"
+                                    id="txtNome" placeholder="Informe o nome"
+                                    required
+                                    name="nome"
+                                    value={objeto.nome}
+                                    onChange={handleChange} />
+                                <div className="valid-feedback">
+                                    Campo nome OK
+                                </div>
+                                <div className="invalid-feedback">
+                                    Informe o nome
+                                </div>
+                            </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary"
